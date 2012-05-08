8833a5f01ce6560fc98c8ca0170b876a78f8cef2
Eric Stein <toba@des.truct.org>
2012-05-08 01:08:57 -0500

    search for working groups

--- general/views.py
+++ general/views.py
@@ -130,6 +130,7 @@ def search(request):
         ('Groups', models.Organization.search(q)),
         ('Resources', models.Resource.search(q)),
         ('Skills', models.Skill.search(q)),
+        ('Working Groups', models.WorkingGroup.search(q)),
     ]
 
     results = filter(lambda x: x[1].count() > 0, results)
