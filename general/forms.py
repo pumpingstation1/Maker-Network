6ac4e0bc20d6c76a7f34df83bc0cfb874eab3544
Eric Stein <toba@des.truct.org>
2012-07-18 00:35:24 -0500

    privacy setting - profile only visible to logged in users in the at least one group as the user.

--- general/forms.py
+++ general/forms.py
@@ -14,7 +14,7 @@ class UserProfileForm(forms.ModelForm):
 
         self.fields.keyOrder = [
             'first_name', 'last_name', 'email',
-            'url', 'phone', 'bio', 'interests', 'location', 'avatar', 'bg_image'
+            'url', 'phone', 'bio', 'interests', 'location', 'avatar', 'bg_image', 'privacy_groupsonly'
             ]
 
     def save(self, *args, **kw):
